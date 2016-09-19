4f44a9d87510255eef2dd46c57d184f2bf1fce89
Omri Schwarz <oschwarz@cse.fraunhofer.org>
2016-09-19 14:43:17 -0400

    Works. Peforms type conversions, detects proper date. Inserts.

---
+++ load_isone_csvs.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+import csv
+import pymonetdb
+import sys
+import datetime
+import argparse
+
+
+
+def main(args):
+    """
+Load one of the ISONE historical CSVs into Monetdb
+    """
+    C = pymonetdb.connect("voc")
+    table="lmp_5min"
+    # todo:
+    # open the CSV. Skip the right rows.
+    # do an insert.
+    c=C.cursor()
+    W = csv.reader(open(args[1]))
+    fdate = None
+    for row in W:
+        if ( row[0] == "C" and
+             row[1].startswith("Report for: ")):
+             fdate = datetime.datetime.strptime(
+                 row[1][:24],
+                 "Report for: %m/%d/%Y -").date()
+        if row[0] == "D":
+            row[2] = pymonetdb.sql.monetize.convert(
+                datetime.datetime.combine(
+                    fdate,
+                    pymonetdb.py_time(row[2])).isoformat())
+            print row
+            query = "insert into %s values (%s);"%(
+                table,
+                ','.join(row[1:]))
+            print query
+            c.execute(query)
+
+                
+if __name__ == "__main__":
+    main(sys.argv)
+
+        
+    
