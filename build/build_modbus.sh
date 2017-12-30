c0601dde020d5a5a92a1db2121305a0f06e9d1aa
Matt Kromer <mkromer@cse.fraunhofer.org>
2017-12-30 14:23:34 -0500

    files for SEL testing

--- build/build_modbus.sh
+++ build/build_modbus.sh
@@ -6,7 +6,11 @@ volttron-ctl install --tag mb ~/.volttron/packaged/master_driveragent-3.1.1-py2-
 volttron-ctl config delete platform.driver --all
 volttron-ctl config store platform.driver config ../../../core/MasterDriverAgent/config
 volttron-ctl config store platform.driver devices/Site1 ../cfg/Modbus/demo_site_devicefile
-volttron-ctl config store platform.driver registry_configs/site1_registry.csv ../cfg/Modbus/DemoRegistry.csv --csv
+#volttron-ctl config store platform.driver registry_configs/site1_registry.csv ../cfg/Modbus/ShirleyCore.csv --csv
+volttron-ctl config store platform.driver registry_configs/site1_registry.csv ../cfg/Modbus/CtrlCore.csv --csv
+
+#volttron-ctl config store platform.driver registry_configs/site1_registry.csv ../cfg/Modbus/DemoRegistry.csv --csv
+
 #volttron-ctl config store platform.driver devices/Shirley-MA/South/PMC ./services/core/MasterDriverAgent/devices/shirley-ma-south-pmc
 #volttron-ctl config store platform.driver registry_configs/SOUTH.csv ./services/core/MasterDriverAgent/registry_configs/SOUTH.csv --csv
 #volttron-ctl config store platform.driver devices/Shirley-MA/North/PMC ./services/core/MasterDriverAgent/devices/shirley-ma-north-pmc
--- build/pkg_SiteManager.sh
+++ build/pkg_SiteManager.sh
@@ -1,7 +1,9 @@
 cp ../cfg/SiteCfg/SiteConfiguration-1site.json ~/.volttron/gs_cfg/SiteConfiguration.json
 cp ../cfg/SystemCfg/SundialSystemConfiguration.json ~/.volttron/gs_cfg/SundialSystemConfiguration.json
 cp ../cfg/DataMap/ShirleySouth-Forecast-data-map.csv ~/.volttron/gs_cfg/
-cp ../cfg/DataMap/ShirleySouth-Modbus-data-map.csv ~/.volttron/gs_cfg/
+#cp ../cfg/DataMap/ShirleySouth-Modbus-data-map.csv ~/.volttron/gs_cfg/
+#cp ../cfg/DataMap/ShirleySouth-Core-Modbus-data-map.csv ~/.volttron/gs_cfg/ShirleySouth-Modbus-data-map.csv
+cp ../cfg/DataMap/ShirleySouth-CtrlCore-Modbus-data-map.csv ~/.volttron/gs_cfg/ShirleySouth-Modbus-data-map.csv
 
 cp ../lib/DERDevice.py ../../../../env/lib/python2.7/site-packages/
 cp ../lib/gs_identities.py ../../../../env/lib/python2.7/site-packages/
