fc864abc94384a81f162e98bb28ab43071dba81c
Matt Kromer <mkromer@cse.fraunhofer.org>
2018-02-27 16:37:59 -0500

    moved sim_start_day and sim_start_hr to gs_identities.  Made forecast inputs negative by convention. (tmp fix). Added additional forecast sim data file

--- ForecastSim/recompile.sh
+++ ForecastSim/recompile.sh
@@ -1,4 +1,5 @@
-cp SAM_PVPwr_nyc.csv  ~/.volttron/gs_cfg/SAM_PVPwr_nyc.csv
+#cp SAM_PVPwr_nyc.csv  ~/.volttron/gs_cfg/SAM_PVPwr_nyc.csv
+cp irr_1min.csv  ~/.volttron/gs_cfg/irr_1min.csv
 volttron-ctl remove  --tag cpr
 volttron-pkg package .
 cp forecast_sim/cpr_ghi.pkl ~/.volttron/gs_cfg/cpr_ghi.pkl
