f794ebdd5d39d2f4600403515ecc1266a7831054
Matt Kromer <mkromer@cse.fraunhofer.org>
2018-04-09 17:15:27 -0400

    Add Load device to DERDevice framework

--- build/pkg_SiteManager.sh
+++ build/pkg_SiteManager.sh
@@ -1,10 +1,11 @@
-cp ../cfg/SiteCfg/SiteConfiguration-1site.json ~/.volttron/gs_cfg/SiteConfiguration.json
-#cp ../cfg/SiteCfg/SiteConfiguration-1siteAndDemand.json ~/.volttron/gs_cfg/SiteConfiguration.json
+#cp ../cfg/SiteCfg/SiteConfiguration-1site.json ~/.volttron/gs_cfg/SiteConfiguration.json
+cp ../cfg/SiteCfg/SiteConfiguration-1siteAndDemand.json ~/.volttron/gs_cfg/SiteConfiguration.json
 #cp ../cfg/SiteCfg/SiteConfiguration-Demand.json ~/.volttron/gs_cfg/SiteConfiguration.json
 cp ../cfg/SystemCfg/SundialSystemConfiguration.json ~/.volttron/gs_cfg/SundialSystemConfiguration.json
 cp ../cfg/DataMap/ShirleySouth-Forecast-data-map.csv ~/.volttron/gs_cfg/
 cp ../cfg/DataMap/ShirleySouth-Modbus-data-map.csv ~/.volttron/gs_cfg/
 cp ../cfg/DataMap/FLAME-Forecast-data-map.csv ~/.volttron/gs_cfg/
+cp ../cfg/DataMap/FLAME-LoadReport-data-map.csv ~/.volttron/gs_cfg/
 
 cp ../lib/DERDevice.py ../../../../env/lib/python2.7/site-packages/
 cp ../lib/gs_identities.py ../../../../env/lib/python2.7/site-packages/
