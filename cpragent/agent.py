6a363c77d6c7d692ffb2d10f7e1090b07515a571
Cameron Stark <cstark@cse.fraunhofer.org>
2018-04-20 14:11:05 -0600

    separate cpr model request and receive methods

--- cpragent/agent.py
+++ cpragent/agent.py
@@ -6,7 +6,7 @@ import datetime
 from datetime import timedelta
 import pytz
 from xml.dom import minidom
-import time 
+import time
 import xml.etree.ElementTree as ET
 
 from volttron.platform.vip.agent import Agent, PubSub, Core
@@ -26,8 +26,9 @@ def CPRPub(config_path,**kwargs):
     conf = utils.load_config(config_path)
     test = utils.load_config(config_path)
     query_interval = conf.get("interval")
+    receive_interval = 5
     userName = conf.get("userName")
-    password = conf.get("password")    
+    password = conf.get("password")
     querystring = conf.get("querystring")
     url = conf.get("url")
     url2 = conf.get("url2")
@@ -46,19 +47,20 @@ def CPRPub(config_path,**kwargs):
     _log.info("Config variables-topic {}".format(topic))
     _log.info("Config variables-horizon {}".format(horizon))
     _log.info("STEP 1")
-      
+
     def get_date():
 	    dt_strt = datetime.datetime.isoformat(datetime.datetime.now(tz=pytz.timezone('America/New_York')).replace(microsecond=0,second=0,minute=0)+ timedelta(hours=1))
             dt_end  = datetime.datetime.isoformat(datetime.datetime.now(tz=pytz.timezone('America/New_York')).replace(microsecond=0,second=0,minute=0)+timedelta(hours=6))
-            
-	    return dt_strt, dt_end 
-	
+
+	    return dt_strt, dt_end
+
     class CPRAgent(Agent):
 	_log.info("Entering Agent class - STEP 2")
 	def __init__(self,config_path,**kwargs):
             super(CPRAgent,self).__init__(**kwargs)
-            self.initialization_complete = 0
+            self.initialization_complete = False
             self.cnt=0
+            self.process_times = []
             _log.info("Initializing - STEP 3")
 
 
@@ -105,14 +107,14 @@ EndTime="{1}"
 SpatialResolution_Degrees="0.01"
 TimeResolution_Minutes="60"/>
  </SimulationOptions>
-</CreateSimulationRequest>""", 
+</CreateSimulationRequest>""",
             "headers": {'content-type': "text/xml; charset=utf-8",
                         'content-length': "length",},
 		#"topic": "devices/cpr",
-	   
+
             }
- 
-            self.payload_base = self.default_config["payload"]          
+
+            self.payload_base = self.default_config["payload"]
 
             self.init_config = self.default_config.copy()
             self.init_config["payload"] = self.init_config["payload"].format(datetime.datetime.isoformat(self.start_date), datetime.datetime.isoformat(self.end_date))
@@ -126,9 +128,9 @@ TimeResolution_Minutes="60"/>
 
         def configure(self,config_name, action, contents):
            _log.info("configuring method - STEP 4")
-           
+
            conf.update(contents)
-           
+
            _log.info("conf {}".format(conf))
            _log.info("CONF")
 
@@ -137,14 +139,15 @@ TimeResolution_Minutes="60"/>
                pass
            except ValueError as e:
                _log.error("ERROR PROCESSING CONFIGURATION: {}".format(e))
-           self.initialization_complete = 1           
+           self.initialization_complete = True
+           self.status_pending = False
 
         def parse_query(self,query):
             """ Function to parse XML response from API"""
             _log.info("Parse query - STEP 5")
             xmldoc = minidom.parseString(query)
             SimPd = xmldoc.getElementsByTagName('SimulationPeriod')
-            results ={"results":[[sim.attributes['StartTime'].value,float(sim.attributes['PowerAC_kW'].value)] 
+            results ={"results":[[sim.attributes['StartTime'].value,float(sim.attributes['PowerAC_kW'].value)]
 			for sim in SimPd],
                     "Units":"kWh",
                     "tz": "UTC-5",
@@ -152,81 +155,68 @@ TimeResolution_Minutes="60"/>
                 }
             return results
 
-        #@Core.periodic(period = 2)   
-        #def test_msg(self):
-         #   _log.info("cnt = "+str(self.cnt))
-          #  self.cnt+=1
-        
 
-        @Core.periodic(period = query_interval)  
-                  
-        def query_cpr(self):
-            status_pending = 1
-            time_request = 0
-            time_now = 0
+        @Core.periodic(period = query_interval)
+        def request_cpr_model(self):
 
+            if self.initialization_complete == True:     # queries server only after config steps are completed
 
-            #self.start_date = self.start_date+timedelta(seconds=3600)
-            #self.end_date   = self.end_date+timedelta(seconds=3600)
-            self.start_date, self.end_date = get_date()
-
-
-
-            _log.info("start date = "+str(self.start_date)+"; end date is "+str(self.end_date))
-
-            if self.initialization_complete == 1:     # queries server only after config steps are completed
-
+                _log.info("Attempting to make a model request")
+                if self.status_pending is not True:
+                    self.start_date, self.end_date = get_date()
+                    _log.info("start date = "+str(self.start_date)+"; end date is "+str(self.end_date))
                     cur_payload = self.payload_base
-                    _log.info("cur payload is {}".format(cur_payload))
-
                     cur_payload = cur_payload.format(self.start_date, self.end_date) #datetime.datetime.isoformat(self.start_date), datetime.datetime.isoformat(self.end_date))
-                    _log.info("new payload is {}".format(cur_payload))
 
-
-                    _log.info("SEE ME 1")
+                    _log.info("Making model request:\n{}".format(cur_payload))
                     response = requests.post(conf['url'],
-		                         auth = HTTPBasicAuth(conf['userName'],conf['password']),
-		                         data= cur_payload, #self.default_config['payload'],
-		                         headers=self.default_config['headers'],
-		                         params=conf['querystring'])
-                    _log.info("Request Status Code{}".format(response.status_code))
+                                             auth = HTTPBasicAuth(conf['userName'],conf['password']),
+                                             data= cur_payload, #self.default_config['payload'],
+                                             headers=self.default_config['headers'],
+                                             params=conf['querystring'])
 
-		    _log.info("GET Request to get sim results - delay required upto 5 mins")
-		    root = ET.fromstring(response.content)
-		    publicId = root.attrib.get("SimulationId")
-		    _log.info("PublicId {}".format(publicId))
-                    time_request = time.time()
-                    _log.info("SEE ME 2 - time request{}".format(time_request))
-		    time.sleep(2)
+                    _log.info("Request Status Code: {}".format(response.status_code))
+                    if response.status_code == 200:
+                        self.status_pending = True
+                        self.simulationId = ET.fromstring(response.content).attrib.get("SimulationId")
+                        self.start_time = time.time()
+                else:
+                    _log.info("Another model has been requested and is pending")
 
 
-                    while (status_pending == 1): #(time_now - time_request <= query_interval):
-                        if status_pending == 1:
-                            _log.info("SEE ME 3")                      
-                            time_now = time.time()
-                            _log.info("time_now {}".format(time_now))
-                            data = requests.get(url2 + publicId, auth = HTTPBasicAuth(userName,password))
-                            root1 = ET.fromstring(data.content)
-                            status = root1.attrib.get("Status")
-                            _log.info("Get Request Status {}".format(status))
-                            if status == "Pending":
-                                status_pending = 1
-                                time.sleep(2)
-                                _log.info("SEE ME 4")
-                                time_now = time.time()
-                            
-                            else:
-                                status_pending = 0
-                                _log.info("SEE ME 5")
-                                parsed_response = self.parse_query(data.content)
-		                _log.info("Parsed Response {}".format(parsed_response))
-		                self.vip.pubsub.publish(
-		                peer="pubsub",
-		                    topic=conf['topic'],
-		                    headers={},
-		                    message=parsed_response)
-                        
-                                                       
+        @Core.periodic(period = receive_interval)
+        def receive_cpr_model(self):
+
+            if (self.initialization_complete == True) and (self.status_pending == True):
+
+                url2 =  "https://service.solaranywhere.com/api/v2/SimulationResult/" + self.simulationId
+                data = requests.get(url2,
+                                    auth = HTTPBasicAuth(conf['userName'], conf['password'])
+                                    )
+                status = ET.fromstring(data.content).attrib.get('Status')
+
+                if status == 'Done':
+                    parsed_response = self.parse_query(data.content)
+
+                    _log.info("Model received, Parsed Response Sending: {}".format(parsed_response))
+                    self.vip.pubsub.publish(
+                        peer="pubsub",
+                        topic=conf['topic'],
+                        headers={},
+                        message=parsed_response)
+
+                    self.status_pending = False # allow new model requests to be made
+
+                    # receive_interval optimization
+                    request_process_time = round(time.time() - self.start_time, 2)
+                    _log.info("Request process time was: {}".format(request_process_time))
+                    self.process_times.append(request_process_time)
+                    receive_interval = sum(self.process_times)/len(self.process_times)
+
+
+                elif status == 'Pending':
+                    _log.info("model still pending, waiting...")
+
 
     CPRAgent.__name__="CPRPub"
     return CPRAgent(config_path,**kwargs)
@@ -238,7 +228,7 @@ def main(argv=sys.argv):
     utils.vip_main(CPRPub)
 
 
-    
+
 if __name__ == '__main__':
     # Entry point for script
     try:
