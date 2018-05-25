ddc71a6a17562c4ce03b8e98ee0d07db78637c6c
Cameron Stark <cstark@cse.fraunhofer.org>
2018-05-25 16:48:43 -0600

    Implement LoadSelect, LoadReport, Status

--- flame/FLAME.py
+++ flame/FLAME.py
@@ -253,6 +253,8 @@ class IPKeys(object):
         self.ws.send(self.request)
         _log.info("Request sent")
 
+        # TODO: add a means of confirming that the request was received (200?)
+
         # (3) check for response
         _log.info("Receiving")
         result_json = self.ws.recv()
@@ -360,6 +362,81 @@ class LoadShift(IPKeys):
 
         return None
 
+class LoadSelect(IPKeys):
+    def __init__(self, websocket, optionID):
+        IPKeys.__init__(self, websocket)
+
+        self.type = u'LoadSelectResponse'
+
+        self.request = json.dumps({'type': 'LoadSelectRequest',
+                                            'msg': {"optionID": optionID}
+                                            }
+                                           )
+        return None
+
+    def __repr__(self):
+        return ('\n'.join(['%s(' % self.__class__.__name__,
+                           # '%s,' % self.start,
+                           # '%s,' % self.granularity,
+                           # '%s)' % self.duration,
+                           ]))
+    def process(self):
+
+        self._send_receive()
+        self.status = self.response['msg']['status']
+
+class LoadReport(IPKeys):
+    def __init__(self, websocket, dstart, sampleInterval, duration):
+        IPKeys.__init__(self, websocket)
+
+        self.type = u'LoadReportResponse'
+
+        self.request = json.dumps({'type': 'LoadReportRequest',
+                                   'msg': {
+                                       "dstart": dstart,        #start time for report
+                                       "sampleInterval": sampleInterval,            #sample interval
+                                       "duration": duration            # duration of request
+                                       }
+                                   }
+                                   )
+        return None
+
+    def __repr__(self):
+        return ('\n'.join(['%s(' % self.__class__.__name__,
+                           # '%s,' % self.start,
+                           # '%s,' % self.granularity,
+                           # '%s)' % self.duration,
+                           ]))
+    def process(self):
+
+        self._send_receive()
+        self.loadSchedule = pd.DataFrame(self.response['msg']['loadSchedule'])
+
+class Status(IPKeys):
+    def __init__(self, websocket):
+        IPKeys.__init__(self, websocket)
+
+        self.type = u'StatusResponse'
+
+        self.request = json.dumps({'type': 'StatusRequest',
+                                   'msg': {
+                                       }
+                                   }
+                                   )
+        return None
+
+    def __repr__(self):
+        return ('\n'.join(['%s(' % self.__class__.__name__,
+                           # '%s,' % self.start,
+                           # '%s,' % self.granularity,
+                           # '%s)' % self.duration,
+                           ]))
+    def process(self):
+
+        self._send_receive()
+        # self.loadSchedule = pd.DataFrame(self.response['msg']['loadSchedule'])
+        self.alertStatus = self.response['msg']['alertStatus']
+        self.currentProfile = self.response['msg']['currentProfile']
 # helper functions
 def create_baseline_request(start, granularity, duration):
     baseline_request = json.dumps({'type': 'BaselineRequest',
@@ -380,8 +457,7 @@ def create_load_request():
     gs_root_dir = os.environ['GS_ROOT_DIR']
     flame_path  = "FLAME-v2/flame/"
     fname       = 'defaultLoadRequest.json'
-    filepath    = gs_root_dir+flame_path+fname
-    #filepath = '/home/cstark/bin/volttron/services/contrib/GlobalSchedulerCore/FLAME/flame/defaultLoadRequest.json'
+    filepath    = os.path.join(gs_root_dir, flame_path, fname)
 
     with open(filepath) as f:
         msg = json.load(f)
@@ -426,12 +502,40 @@ if __name__ == '__main__':
     bl = Baseline(start, granularity, duration, ws)
     print("processing Baseline")
     bl.process()
-    print(bl.forecast)
+    print("Here's the Baseline forecast:\n", bl.forecast)
     print("done processing Baseline")
 ##
     print("running LoadShift")
     # LoadShift
     ls = LoadShift(ws)
     ls.process()
-    print(ls.forecast)
+    print("Here's the LoadShift forecast:\n", ls.forecast)
+    print("done processing LoadShift")
+    ##
+    print("running LoadSelect")
+    lsel = LoadSelect(ws, 1)
+    lsel.process()
+    # print("Here's the LoadSelect response:\n", lsel.response)
+    print("Here's the LoadSelect status:\n", lsel.status)
+    print("done processing LoadSelect")
+    ##
+    print("running LoadReport")
+    loadReport_kwargs = {
+        "dstart": "2018-01-01T00:00:00",        #start time for report
+        "sampleInterval": "PT1H",            #sample interval
+        "duration": "PT24H"            # duration of request
+    }
+    lr = LoadReport(ws, **loadReport_kwargs)
+    lr.process()
+    # print("Here's the LoadReport response:\n", lr.response)
+    print("Here's the LoadReport loadSchedule:\n", lr.loadSchedule)
+    print("done processing LoadReport")
+    ##
+    print("running Status")
+    status = Status(ws)
+    status.process()
+    # print("Here's the Status response:\n", status.response)
+    print("Here's the Status alertStatus:\n", status.alertStatus)
+    print("Here's the Status currentProfile:\n", status.currentProfile)
+    print("done processing Status")
     ##
