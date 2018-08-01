457c3ddd7ec808c8ab8f6bb05f13561c9214363c
Matt Kromer <mkromer@cse.fraunhofer.org>
2018-08-01 13:15:09 -0400

    added ability to specify data map from a cfg json file.  This makes it so that switching plant vs device mode control can be done entirely from the cfg file.

--- build/pkg_SiteManager.sh
+++ build/pkg_SiteManager.sh
@@ -4,6 +4,4 @@ cp ../lib/gs_utilities.py ../../../../env/lib/python2.7/site-packages/
 cp ../lib/HistorianTools.py ../../../../env/lib/python2.7/site-packages/
 cp ../lib/UnitConversion.py ../../../../env/lib/python2.7/site-packages/
 
-#cp ../cfg/DataMap/ShirleySouthDeviceLevel-Modbus-data-map.csv ../cfg/DataMap/ShirleySouth-Modbus-data-map.csv
-cp ../cfg/DataMap/ShirleySouthPlantLevel-Modbus-data-map.csv ../cfg/DataMap/ShirleySouth-Modbus-data-map.csv
 volttron-pkg package ../SiteManager
