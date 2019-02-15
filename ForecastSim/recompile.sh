696539e6e58925e3a97b37f493ae89769cedd049
Matt Kromer <mkromer@cse.fraunhofer.org>
2019-02-15 16:57:47 -0500

    modified forecast sim module to accept historical forecasts as stored in the database.  Moved logic into a separate forecast class.

--- ForecastSim/recompile.sh
+++ ForecastSim/recompile.sh
@@ -5,6 +5,9 @@ cp NYC_demand_avg.csv  ~/.volttron/gs_cfg/NYC_demand.csv
 cp NYC_demand_interpolated.csv  ~/.volttron/gs_cfg/NYC_demand_interpolated.csv
 cp NYC_forecast_interpolated.csv  ~/.volttron/gs_cfg/NYC_forecast_interpolated.csv
 cp irr_1min.csv  ~/.volttron/gs_cfg/irr_1min.csv
+cp solar_forecast_matrix.csv  ~/.volttron/gs_cfg/
+cp solar_forecast_ts_matrix.csv ~/.volttron/gs_cfg/
+
 volttron-ctl remove  --tag cpr
 volttron-pkg package .
 cp forecast_sim/cpr_ghi.pkl ~/.volttron/gs_cfg/cpr_ghi.pkl
